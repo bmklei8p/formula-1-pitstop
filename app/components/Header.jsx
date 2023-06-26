4693e58f1e716106021c3f45d196aa9c6977f73a
bryan <bmklei8p@gmail.com>
2023-06-26 12:12:27 -0400

    finished functionality and responsiveness of navbar. small updates to ui needed noted in readme

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,91 +1,130 @@
 'use client'
 
-import React, { useState } from 'react';
-import Link from 'next/link';
+import { useState } from 'react'
+import React from 'react'
 
-const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+import Link from 'next/link'
+import Image from 'next/image'
+import { RiMenuFill } from 'react-icons/ri'
+import { BiSolidBarChartAlt2, BiSolidCalendar } from 'react-icons/bi'
+import { GiFullMotorcycleHelmet } from 'react-icons/gi'
+import { PiFlagCheckeredFill } from 'react-icons/pi'
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+const Header = () => {
+  const [showSideBar, setShowSideBar] = useState(false)
 
-  return (
-    <nav className="flex w-full items-center justify-between bg-red-500 p-4">
-      <div className="flex items-center">
-        <img src="/path/to/logo.png" alt="Logo" className="h-8 mr-4" />
-      </div>
-      <div>
-        <h3 className="text-white font-bold text-xl">Formula 1</h3>
-      </div>
-      <div className="flex items-center">
-        <ul className="hidden sm:flex space-x-4">
-          <li>
-            <Link href="/schedule" className='text-white'>
-              Schedule
-            </Link>
-          </li>
-          <li>
-            <Link href="/standings" className='text-white'>
-              Standings
-            </Link>
-          </li>
-          <li>
-            <Link href="/teams" className='text-white'>
-              Teams
-            </Link>
-          </li>
-          <li>
-            <Link href="/drivers" className="text-white"> 
-                Drivers
-            </Link>
-          </li>
-          <li>
-            <Link href="/tracks" className="text-white">
-              Tracks
-            </Link>
-          </li>
-        </ul>
-        <button
-          className="sm:hidden text-white ml-4 focus:outline-none"
-          onClick={toggleMenu}
-        >
-          Menu
-        </button>
-      </div>
-      {isMenuOpen && (
-        <div className="sm:hidden bg-red-500 p-4 absolute top-16 right-0 z-50">
-          <ul className="space-y-2">
-            <li>
-              <Link href="/schedule" className="text-white block"> 
-                Schedule
-              </Link>
-            </li>
-            <li>
-              <Link href="/standings" className="text-white block">  
-                Standings
-              </Link>
-            </li>
-            <li>
-              <Link href="/teams" className="text-white block"> 
-                Teams
-              </Link>
-            </li>
-            <li>
-              <Link href="/drivers" className="text-white block">
-                Drivers
-              </Link>
-            </li>
-            <li>
-              <Link href="/tracks" className="text-white block">
-                Tracks
-              </Link>
-            </li>
-          </ul>
+  const navLinks = [{
+    title: "Schedule",
+    href: '/schedule',
+    icon: <BiSolidCalendar />,
+    }, {
+    title: "Standings",
+    href: '/standings',
+    icon: <BiSolidBarChartAlt2 />,
+    }, {
+    title: "Drivers",
+    href: '/drivers',
+    icon: <GiFullMotorcycleHelmet />,    
+    }, {
+    title: "Teams",
+    href: '/teams',
+    icon: <PiFlagCheckeredFill />,    
+    }, {
+    title: "Tracks",
+    href: '/tracks',
+    icon: "",
+    img: '/assets/images/race-track-icon.svg',
+    }, 
+]
+
+return (
+    // desktop header
+    <div className='header-container'>
+        <div className="header-box">
+            <div className="logo">
+                <Image className="logo-img" src='/assets/images/logo.png' alt='f1-logo' width={50} height={50} />
+                <h2 className={showSideBar ? "hidden" : "title"}>Formula 1 Pitstop</h2>
+            </div>
+            <div className="md:flex hidden" style={{padding: "1rem"}}>
+                <div className='links'>
+                {navLinks.map((link) => (
+                        <div key={link.href} className='desktop-link-box'>
+                            <Link href={link.href}>{link.title}</Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            {/* mobile header - sidebar */}
+            <div className="md:hidden flex justify-end">
+                <RiMenuFill size={30} onClick={() => setShowSideBar((prev) => !prev)} />
+                {showSideBar && (
+                    <div className="sidebar">
+                        <div className="sidebar-header">
+                            <Image className="logo-img" src='/assets/images/logo.png' alt='f1-logo' width={50} height={50} />
+                            <h2 className="sidebar-title">Formula 1 Pitstop</h2>
+                        </div>
+                        <div className="sidebar-links">
+                            {navLinks.map((link) => (
+                            <div key={link.href} className="sidebar-link-block">
+                                <div className="sidebar-link-box">
+                                    {link.icon && React.cloneElement(link.icon, { size: 25 })}
+                                    {link.img && <Image src={link.img} alt='f1-logo' width={25} height={25} />}
+                                </div>
+                                <Link href={link.href}>
+                                    {link.title}
+                                </Link>
+                            </div>
+                            ))}
+                        </div>
+                        <div className="sidebar-close">
+                            <button className="btn btn-primary" onClick={() => setShowSideBar(false)}>Close</button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
-      )}
-    </nav>
-  );
-};
+    </div>
+  )
+}
 
-export default Navbar;
+
+// mobile menu with 
+//  <RiMenuFill size={30} onClick={() => setShowSideBar((prev) => !prev)} />
+    // {showSideBar && (
+    //     <div className="mobile-menu">
+    //         {navLinks.map((link) => (
+    //             <Link key={link.href} href={link.href}>{link.title}</Link>
+    //         ))}
+    //         <div>
+    //             <button className="btn btn-primary" onClick={() => setShowSideBar((prev) => !prev)}>Close</button>
+    //         </div>
+    //     </div>
+    // )}
+
+
+// Header with icon left, title center, and links right but not working with responsive:
+// the title is not centered when the screeen is smaller than a large desktop   uses flex-basis-30/40/30
+//   return (
+//     // desktop header
+//     <div className='header-container'>
+//         <div className="header-box">
+//             <div className="logo">
+//                 <Image src='/assets/images/logo.png' alt='f1-logo' width={45} height={45} />
+//             </div>
+//             <div className="md:flex hidden">
+//                 <h2 className="title">Formula 1 Pitstop</h2>
+//             </div>
+//             <div className="md:flex hidden" >
+//                 <div className='flex-basis-30 flex justify-end gap-15'>
+//                 {navLinks.map((link) => (
+//                         <Link key={link.href} href={link.href}>{link.title}</Link>
+//                     )
+//                 )}
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+export default Header
