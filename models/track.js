f2829b0c4ae952ef1315abc28677c027fe8bee7e
bryan <bmklei8p@gmail.com>
2023-06-29 13:19:46 -0400

    updating get requests for track and tracks

--- models/track.js
+++ models/track.js
@@ -1,17 +1,61 @@
 import {Schema, model, models} from "mongoose";
 
 const TrackSchema = new Schema({
-    creator: {
-        type: Schema.Types.ObjectId,
-    },
-    prompt: {
+    officialName: {
         type: String,
-        required: [true, "Please provide a prompt"],
+        required: [true, "Please provide the official name"],
     },
-    tag: {
+    friendlyName: {
         type: String,
-        required: [true, "Please provide a tag"],
-    }
+        required: [true, "Please provide a user friendly name"],
+    },
+    trackStartDate: {
+        type: String,
+        required: [true, "Please provide the date the track was first used"],
+    },
+    numberOfLaps: {
+        type: Number,
+    },
+    lapDistance: {
+        type: Number,
+    },
+    fastestLapTime: {
+        type: String,
+    },
+    fastestLapDriver: {
+        type: String,
+    },
+    fastestLapDriver: {
+        type: String,
+    },
+    fastestLapYear: {
+        type: String,
+    },
+    fastestLapTeam: {
+        type: String,
+    },
+    inCurrentSeason: {
+        type: Boolean,
+        required: [true, "Please provide if the track is in the current season as a yes or no"],
+    },
+    trackLayoutImage: {
+        type: String,
+    },
+    historicalImages: {
+        type: Array,
+    },
+    firstParagraph: {
+        type: String,
+        required: [true, "Please provide a short description of history of the track"],
+    },
+    secondParagraph: {
+        type: String,
+        required: [true, "Please provide a long description of the track"],
+    },
+    thirdParagraph: {
+        type: String,
+        required: [true, "Please provide a description of the track"],
+    },
 });
 
 const Track = models.Track || model("Track", TrackSchema);
