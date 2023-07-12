43ddd87f1b4b0fa5cc6a65dee4027d3b19ed7d03
Bryan Kleinberg <bmkleip@gmail.com>
2023-07-11 21:42:00 -0400

    updated podium images

--- app/layout.js
+++ app/layout.js
@@ -15,45 +15,45 @@ const titilliumWeb = Titillium_Web({
   style: 'normal',
   subsets: ['latin'],
   display: 'swap',
-  variable: 
+  variable:
     '--font-titillium-web',
 })
 
 export default function RootLayout({ children }) {
-  const initialConnectToDB = async () => {
-    mongoose.set('strictQuery', true);
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "f1-pitstop",
-            useNewURLParser: true,
-            useUnifiedTopology: true,
-        })
-  
-        // upload all tracks to database
-        // check if tracks collection is empty
-        const data = await mongoose.connection.collection('tracks').find({}).toArray();
-        if (data.length > 0) {
-            console.log("Tracks already uploaded to database");
-            return;
-        }
-        const tracks = require('../utils/tracks.json');
-        await mongoose.connection.collection('tracks').insertMany(tracks);
-        console.log("Tracks uploaded to database");
-        // upload all drivers to database
-        // const drivers = require('./drivers.json');
-        // await mongoose.connection.collection('drivers').insertMany(drivers);
-        // console.log("Drivers uploaded to database");
-        // // upload all teams to database
-        // const teams = require('./teams.json');
-        // await mongoose.connection.collection('teams').insertMany(teams);
-        // console.log("Teams uploaded to database");
-    } catch (error) {
-        console.log(error);
-    }
-  }
+  // const initialConnectToDB = async () => {
+  //   mongoose.set('strictQuery', true);
+  //   try {
+  //       await mongoose.connect(process.env.MONGODB_URI, {
+  //           dbName: "f1-pitstop",
+  //           useNewURLParser: true,
+  //           useUnifiedTopology: true,
+  //       })
 
-  initialConnectToDB();
-  
+  //       // upload all tracks to database
+  //       // check if tracks collection is empty
+  //       const data = await mongoose.connection.collection('tracks').find({}).toArray();
+  //       if (data.length > 0) {
+  //           console.log("Tracks already uploaded to database");
+  //           return;
+  //       }
+  //       const tracks = require('../utils/tracks.json');
+  //       await mongoose.connection.collection('tracks').insertMany(tracks);
+  //       console.log("Tracks uploaded to database");
+  //       // upload all drivers to database
+  //       // const drivers = require('./drivers.json');
+  //       // await mongoose.connection.collection('drivers').insertMany(drivers);
+  //       // console.log("Drivers uploaded to database");
+  //       // // upload all teams to database
+  //       // const teams = require('./teams.json');
+  //       // await mongoose.connection.collection('teams').insertMany(teams);
+  //       // console.log("Teams uploaded to database");
+  //   } catch (error) {
+  //       console.log(error);
+  //   }
+  // }
+
+  // initialConnectToDB();
+
   return (
     <html lang="en" className={`${titilliumWeb.variable}`}>
       <body>
@@ -68,4 +68,3 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
-
