d404d2e05f8e2aad446c9af0fed0b170157d2204
Bryan Kleinberg <bmkleip@gmail.com>
2023-07-11 23:04:18 -0400

    updated race and podiums

--- app/schedule/components/RacePodiumResults.jsx
+++ app/schedule/components/RacePodiumResults.jsx
@@ -1,39 +1,42 @@
 // 'use client'
 import Image from "next/image"
-import Link from "next/link"
-import { IoChevronBackSharp } from "react-icons/io5";
+// import Link from "next/link"
+// import { IoChevronBackSharp } from "react-icons/io5";
 
 const RacePodiumResults = ({raceResults, raceCircuitId}) => {
 
   return (
-    <div className="flex flex-col text-center items-center gap-y-4">
-        <div className="relative" >
-            <Link className="border-2 hover:bg-gray-800 left-0 border-gray-800 text-red-500 absolute" href={"/schedule"}>
+    <div className="flex flex-col text-center items-center">
+        {/* <div className="">
+            <Link className="border-2 hover:bg-gray-400 text-black-500 absolute" href={"/schedule"}>
                 <IoChevronBackSharp size={45} />
             </Link>
+        </div> */}
+        {/* need to put this image in a box for larger screens */}
+        <div className="relative">
             <Image className="" priority={true} fill={false} width={1353} height={447} src={`/assets/images/podium/${raceCircuitId}_podium.jpeg`} alt="podium"/>
         </div>
-        <div className="grid grid-cols-3 w-full text-lg md:text-xl">
-            <div className="justify-self-center">
+        <div className="flex flex-row w-full text-lg md:text-xl">
+            <div className="justify-self-center basis-1/5">
                 <Image src="/assets/images/first_place.png" alt="first place trophy" width={50} height={50} />
             </div>
-            <div>{raceResults[0].Driver.givenName + " " + raceResults[0].Driver.familyName}</div>
-            <div>{raceResults[0].Time.time}</div>
-        </div>           
-        <div className="grid grid-cols-3 w-full text-lg md:text-xl">
-        <div className="justify-self-center">
-                <Image src="/assets/images/first_place.png" alt="first place trophy" width={50} height={50} />
-            </div>
-            <div>{raceResults[1].Driver.givenName + " " + raceResults[1].Driver.familyName}</div>
-            <div>{raceResults[1].Time.time}</div>
+            <div className="basis-3/5 ">{raceResults[0].Driver.givenName + " " + raceResults[0].Driver.familyName}</div>
+            <div className="basis-1/5">{raceResults[0].Time.time.slice(0,7)}</div>
         </div>
-        <div className="grid grid-cols-3 w-full text-lg md:text-xl">
-        <div className="justify-self-center">
+        <div className="flex flex-row w-full text-lg md:text-xl">
+        <div className="justify-self-center basis-1/5">
                 <Image src="/assets/images/first_place.png" alt="first place trophy" width={50} height={50} />
             </div>
-            <div>{raceResults[2].Driver.givenName + " " + raceResults[2].Driver.familyName}</div>
-            <div>{raceResults[2].Time.time}</div>
-        </div>  
+            <div className="basis-3/5">{raceResults[1].Driver.givenName + " " + raceResults[1].Driver.familyName}</div>
+            <div className="basis-1/5">{raceResults[1].Time.time}</div>
+        </div>
+        <div className="flex flex-row w-full text-lg md:text-xl">
+        <div className="justify-self-center basis-1/5">
+                <Image src="/assets/images/first_place.png" alt="first place trophy" width={50} height={50} />
+            </div>
+            <div className="basis-3/5">{raceResults[2].Driver.givenName + " " + raceResults[2].Driver.familyName}</div>
+            <div className="basis-1/5">{raceResults[2].Time.time}</div>
+        </div>
     </div>
   )
 }
--- app/schedule/current/completed/[round]/page.jsx
+++ app/schedule/current/completed/[round]/page.jsx
@@ -1,13 +1,13 @@
 import RacePodiumResults from "@/app/schedule/components/RacePodiumResults"
 import ResultsList from "@/app/schedule/components/ResultsList";
 
-const RaceResultsPage = async ({ params }) => { 
+const RaceResultsPage = async ({ params }) => {
   const race = await fetch(`http://localhost:3000/api/schedule/season/completed/${params.round}`)
     .then((res) => res.json())
   const raceResults = race.Results;
 
   return (
-    <div className="flex flex-col justify-center text-left w-full md:w-3/4 ml-2 mr-2">
+    <div className="flex flex-col justify-center text-left w-full md:w-3/4">
         <RacePodiumResults raceResults={raceResults} raceCircuitId={race.Circuit.circuitId}/>
         <div className="text-xl mt-4 text-red-500 md:text-2xl">Race Weekend Results</div>
         <div>{race.raceName}</div>
