b1a6acf9a4c8e90ee7399d391728f7ee4384a86a
bryan <bmklei8p@gmail.com>
2023-07-17 09:56:19 -0400

    image carousel fully responsive on teams page now

--- app/teams/components/DriverInfoBox.jsx
+++ app/teams/components/DriverInfoBox.jsx
@@ -2,13 +2,13 @@ import Image from "next/image"
 
 const DriverInfoBox = ({ pictureURL, driverName, driverId }) => {
   return (
-    <div className="border-l-2 border-white">
+    <div className="md:border-l-2 border-white">
       <div className="bg-gray-900 object-center relative ">
         <Image priority={true} src={`/assets/images/drivers/${pictureURL}`} alt={`${driverName} headshot`} width={300} height={300} />
         {driverId === "de_vries" ? <Image className="absolute top-8 left-1" src={"/assets/images/red_x.svg"} height={300} width={300} /> : <></>}
       </div>
     {/* driver info box */}
-    <div className="h-36 border-l-2 border-slate-200">
+    <div className="h-36 md:border-l-2 border-slate-200">
       <div className="ml-4 font-light">
         <h3 className="pt-2 text-xl md:text-2xl">{driverName}</h3>
         <div className="overflow-hidden relative w-14 h-10 mt-3">
--- app/teams/components/ImageCarousel.jsx
+++ app/teams/components/ImageCarousel.jsx
@@ -22,16 +22,20 @@ const ImageCarousel = ({ imageURLArray, path }) => {
 
 
   return (
-    <div className="bg-white border-t-2 border-gray-100 max-w-[1400px] h-[780px] w-full m-auto px-2 relative group">
-        <div style={{backgroundImage: `url(${path}${imageURLArray[currentIndex]})`}} className="w-full h-full rounded-2xl bg-no-repeat bg-top bg-contain duration-500">
+    <div className="bg-white border-t-2 border-gray-100 max-w-full m-0 relative group">
+        <Image src={`${path}${imageURLArray[currentIndex]}`} className="object-contain z-0  md:rounded-2xl" fill={false} width={1440} height={608} priority={true} alt="" />
         {/* left arrow */}
-        </div>
         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
             <BsChevronCompactLeft onClick={handlePrev} size={30} />
         </div>
         {/* right arrow */}
         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
             <BsChevronCompactRight onClick={handleNext} size={30} />
+        </div>
+        <div onClick={handlePrev} className="hidden h-full group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] w-1/5 bg-black/10 md:w-2/12 text-white cursor-pointer">
+        </div>
+        {/* right arrow */}
+        <div onClick={handleNext} className="hidden h-full group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-0 w-1/5 bg-black/10 md:w-2/12 text-white cursor-pointer">
         </div>
     </div>
   )
@@ -54,8 +58,8 @@ const ImageCarousel = ({ imageURLArray, path }) => {
 //   );
 // };
 
+//     {/* <div className="bg-white border-t-2 border-gray-100 w-full m-auto px-2 relative group"> */}
+        {/* <div style={{backgroundImage: `url(${path}${imageURLArray[currentIndex]})`}} className="w-full h-auto block rounded-2xl bg-no-repeat bg-top bg-contain duration-500">
+        </div> */}
+
 export default ImageCarousel;
-
-
-        {/* <button className="relative left-20 z-10 h-1/2 w-1/5 text-3xl border-2 border-black" onClick={handlePrev}>Prev</button> */}
-        {/* <button className="relative z-10 h-1/2 right-20 text-3xl border-2 border-black" onClick={handleNext}>Next</button> */}
