f2649d06efa8252e1112b529be3c9970cacd3c64
bryan <bmklei8p@gmail.com>
2023-07-17 13:46:13 -0400

    updating css and driver podium positioning

--- app/drivers/[driverId]/page.jsx
+++ app/drivers/[driverId]/page.jsx
@@ -1,5 +1,6 @@
 import ImageCarousel from "@/app/teams/components/ImageCarousel"
 import DriverInfoBox from "@/app/drivers/components/DriverInfoBox"
+import Image from "next/image"
 
 const DriverDetailPage = async ({ params }) => {
   const driver = await fetch(`http://localhost:3000/api/drivers/${params.driverId}`
@@ -8,9 +9,12 @@ const DriverDetailPage = async ({ params }) => {
 
   return (
     <div>
-      <div>
+      <div className="flex md:flex-row">
         {/* driver image */}
-        <DriverInfoBox driver={driver} />
+        <div className="basis-full">
+          <Image src={`/assets/images/drivers/${driver.driverHeadshotImage}`} width={500} height={500} priority={true} alt={`${driver.familyName}'s headshot image`} />
+        </div>
+        <DriverInfoBox driver={driver} className="basis-full" />
       </div>
       <div>
         {/* <ImageCarousel /> */}
