087cf048e85eabec89fb113cfd32e5adeccee727
bryan <bmklei8p@gmail.com>
2023-07-26 13:43:13 -0400

    added placeholder blur for banner image. scaled down image to decrease file size and decrease lcp time. added sizes property to not have to download largest file size until needed

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,27 @@
-import Image from "next/image"
-import RecentResults from "./components/RecentResults"
-import BriefStandings from "./components/BriefStandings"
+import Image from "next/image";
+import RecentResults from "./components/RecentResults";
+import BriefStandings from "./components/BriefStandings";
+import BannerImageDesktop from '../public/assets/images/home-pit-stop-desktop.png'
+import BannerImageMobile from '../public/assets/images/home-banner-mobile.png'
+
 
 const OverviewPage = async () => {
-  const res = await fetch('https://ergast.com/api/f1/current.json')
-  const data = await res.json()
-  const schedule = data.MRData.RaceTable.Races
+  const res = await fetch("https://ergast.com/api/f1/current.json");
+  const data = await res.json();
+  const schedule = data.MRData.RaceTable.Races;
   const currentDateTime = new Date();
-  const upcomingRaces = []
-  const pastRaces = []
+  const upcomingRaces = [];
+  const pastRaces = [];
   for (let i = 0; i < schedule.length; i++) {
     // results of previous week will not switch over until first practice of the next race weekend
-    const raceDate = new Date(schedule[i].FirstPractice.date + 'T' + schedule[i].FirstPractice.time);
+    const raceDate = new Date(
+      schedule[i].FirstPractice.date + "T" + schedule[i].FirstPractice.time
+    );
 
     if (raceDate > currentDateTime) {
       upcomingRaces.push(schedule[i]);
     } else {
-      pastRaces.unshift(schedule[i]);  //bad time complexity but list is short
+      pastRaces.unshift(schedule[i]); //bad time complexity but list is short
     }
   }
   const nextRace = upcomingRaces[0];
@@ -24,36 +29,60 @@ const OverviewPage = async () => {
   const resultsRace = pastRaces[0];
   const resultsRaceRound = resultsRace.round;
 
-
   return (
     <div className="w-full flex flex-col items-center">
-        <div className="hidden md:flex justify-center items-end lg:items-start relative bg-gray-900 -z-50 ">
-            <div className="h-36 lg:h-56 xl:h-56 w-2/4 absolute z-40 flex flex-col justify-center items-center opacity-95">
-                <h1 className=" text-4xl lg:text-6xl font-bold text-white z-10">Formula 1 Pitstop</h1>
-                <p className="text-white text-3xl lg:text-5xl z-10 ">Your <strong>one-stop</strong> for F1</p>
-            </div>
-            <Image src="/assets/images/home-pit-stop-desktop.png" priority={true} alt="pit-stop" width={2717} height={1100} className="-z-10 opacity-95" />
+      <div className="hidden md:flex justify-center items-end lg:items-start relative bg-gray-900 -z-50 ">
+        <div className="h-36 lg:h-56 xl:h-56 w-2/4 absolute z-40 flex flex-col justify-center items-center opacity-95">
+          <h1 className=" text-4xl lg:text-6xl font-bold text-white z-10">
+            Formula 1 Pitstop
+          </h1>
+          <p className="text-white text-3xl lg:text-5xl z-10 ">
+            Your <strong>one-stop</strong> for F1
+          </p>
         </div>
-        <div className="flex max-h-[90vh] md:hidden overflow-clip relative justify-center items-end bg-gray-900 -z-50 ">
-            <div className="h-26 w-full absolute z-40 flex flex-col justify-end py-2 items-center bg-gradient-to-t opacity-75 to-[#494949] from-[#2b2b2b]">
-                <h1 className="text-white font-bold text-4xl">Formula 1 Pitstop</h1>
-                <p className="text-white text-3xl">Your <strong>one-stop</strong> for F1</p>
-            </div>
-            <Image src="/assets/images/home-pit-stop-desktop-rotated.png" priority={true} alt="pit-stop" width={1100} height={1100} className="-z-10 object-fill" />
+        <Image
+          src={BannerImageDesktop}
+          priority={true}
+          alt="pit-stop"
+          width={2670}
+          height={1100}
+          quality={75}
+          placeholder="blur"
+          className="-z-10 opacity-95"
+          sizes="(min-width: 777px) 100vw"
+        />
+      </div>
+      <div className="flex max-h-[90vh] md:hidden overflow-clip relative justify-center items-end bg-gray-900 -z-50 ">
+        <div className="h-26 w-full absolute z-40 flex flex-col justify-end py-2 items-center bg-gradient-to-t opacity-75 to-[#494949] from-[#2b2b2b]">
+          <h1 className="text-white font-bold text-4xl">Formula 1 Pitstop</h1>
+          <p className="text-white text-3xl">
+            Your <strong>one-stop</strong> for F1
+          </p>
         </div>
+        <Image
+          src={BannerImageMobile}
+          priority={true}
+          alt="pit-stop"
+          width={1100}
+          height={1100}
+          className="-z-10 object-fill"
+          placeholder="blur"
+          sizes="(max-width: 777px) 100vw, 0vw"
+        />
+      </div>
       {/* Content */}
       <div className="w-full xl:w-9/12 flex flex-col gap-y-8 lg:flex-row mt-4 p-2 lg:p-0 gap-x-8 ">
-          <div className="w-full">
-            {/* Recent Results */}
-            <RecentResults raceRound={resultsRaceRound} race={resultsRace} />
-          </div>
-          <div className="w-full">
-            {/* Standings */}
-            <BriefStandings />
-          </div>
+        <div className="w-full">
+          {/* Recent Results */}
+          <RecentResults raceRound={resultsRaceRound} race={resultsRace} />
+        </div>
+        <div className="w-full">
+          {/* Standings */}
+          <BriefStandings />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default OverviewPage
+export default OverviewPage;
--- app/standings/page.jsx
+++ app/standings/page.jsx
@@ -1,82 +1,154 @@
-'use client'
-import { useState, useEffect } from "react"
-import ConstructorPodium from "./components/ConstructorPodium"
-import DriverPodium from "./components/DriverPodium"
-import DriverStandingsTable from "./components/DriverStandingsTable"
-import ConstructorStandingsTable from "./components/ConstructorStandingsTable"
+"use client";
+import { useState, useEffect } from "react";
+import ConstructorPodium from "./components/ConstructorPodium";
+import DriverPodium from "./components/DriverPodium";
+import DriverStandingsTable from "./components/DriverStandingsTable";
+import ConstructorStandingsTable from "./components/ConstructorStandingsTable";
 
 const StandingsPage = () => {
-  const [showDriversStandings, setShowDriversStandings] = useState(true)
+  const [showDriversStandings, setShowDriversStandings] = useState(true);
 
-  const [ driversStandings, setDriversStandings ] = useState([])
-  const [ constructorsStandings, setConstructorsStandings ] = useState([])
+  const [driversStandings, setDriversStandings] = useState([]);
+  const [constructorsStandings, setConstructorsStandings] = useState([]);
 
   // drivers
-  const [ firstPlaceD, setFirstPlaceD ] = useState()
-  const [ secondPlaceD, setSecondPlaceD ] = useState()
-  const [ thirdPlaceD, setThirdPlaceD ] = useState()
+  const [firstPlaceD, setFirstPlaceD] = useState();
+  const [secondPlaceD, setSecondPlaceD] = useState();
+  const [thirdPlaceD, setThirdPlaceD] = useState();
 
   // constructors
-  const [ firstPlaceC, setFirstPlaceC ] = useState()
-  const [ secondPlaceC, setSecondPlaceC ] = useState()
-  const [ thirdPlaceC, setThirdPlaceC ] = useState()
+  const [firstPlaceC, setFirstPlaceC] = useState();
+  const [secondPlaceC, setSecondPlaceC] = useState();
+  const [thirdPlaceC, setThirdPlaceC] = useState();
 
   useEffect(() => {
     const getDriverStandings = async () => {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/standings/drivers`)
-        const data = await res.json()
-        setDriversStandings(data)
-        setFirstPlaceD(data[0].Driver)
-        setSecondPlaceD(data[1].Driver)
-        setThirdPlaceD(data[2].Driver)
-        }
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/standings/drivers`
+      );
+      const data = await res.json();
+      setDriversStandings(data);
+      setFirstPlaceD(data[0].Driver);
+      setSecondPlaceD(data[1].Driver);
+      setThirdPlaceD(data[2].Driver);
+    };
     getDriverStandings();
-    }, []);
+  }, []);
 
   useEffect(() => {
     const getConstructorsStandings = async () => {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/standings/constructors`)
-        const data = await res.json()
-        setConstructorsStandings(data)
-        setFirstPlaceC(data[0].Constructor?.constructorId)
-        setSecondPlaceC(data[1].Constructor?.constructorId)
-        setThirdPlaceC(data[2].Constructor?.constructorId)
-        }
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/standings/constructors`
+      );
+      const data = await res.json();
+      setConstructorsStandings(data);
+      setFirstPlaceC(data[0].Constructor?.constructorId);
+      setSecondPlaceC(data[1].Constructor?.constructorId);
+      setThirdPlaceC(data[2].Constructor?.constructorId);
+    };
     getConstructorsStandings();
-    }, []);
-
+  }, []);
 
   return (
     <div className="standings-container">
-        <h1 className="hidden text-4xl font-bold mb-4 md:block">{showDriversStandings ? "Drivers Standings": "Constructors Standings"}</h1>
-        {/* mobile buttons */}
-        <div className="flex justify-center md:hidden">
-          <div className="basis-1/2" onClick={() => setShowDriversStandings(true)}>
-            <button className={`${showDriversStandings ? `text-2xl border-b-4 w-full border-red-500 rounded` : `text-xl`}`}>Drivers</button>
-          </div>
-          <div className="basis-1/2" onClick={() => setShowDriversStandings(false)}>
-            <button className={`${!showDriversStandings ? `text-2xl border-b-4 w-full border-red-500 rounded` : `text-xl`}`}>Constructors</button>
-          </div>
+      <h1 className="hidden text-4xl font-bold mb-4 md:block">
+        {showDriversStandings ? "Drivers Standings" : "Constructors Standings"}
+      </h1>
+      {/* mobile buttons */}
+      <div className="flex justify-center md:hidden">
+        <div
+          className="basis-1/2"
+          onClick={() => setShowDriversStandings(true)}
+        >
+          <button
+            className={`${
+              showDriversStandings
+                ? `text-2xl border-b-4 w-full border-red-500 rounded`
+                : `text-xl`
+            }`}
+          >
+            Drivers
+          </button>
         </div>
-        {/* desktop buttons */}
-        <div className="hidden justify-center md:flex">
-          <div className="basis-1/2" onClick={() => setShowDriversStandings(true)}>
-            <button className={`${showDriversStandings ? `text-2xl border-b-4 w-full border-red-500 rounded` : `text-xl`}`} onClick={() => setShowDriversStandings(true)}>Drivers</button>
-          </div>
-          <div className="basis-1/2" onClick={() => setShowDriversStandings(false)}>
-            <button className={`${!showDriversStandings ? `text-2xl border-b-4 w-full border-red-500 rounded` : `text-xl`}`} onClick={() => setShowDriversStandings(false)}>Constructors</button>
-          </div>
+        <div
+          className="basis-1/2"
+          onClick={() => setShowDriversStandings(false)}
+        >
+          <button
+            className={`${
+              !showDriversStandings
+                ? `text-2xl border-b-4 w-full border-red-500 rounded`
+                : `text-xl`
+            }`}
+          >
+            Constructors
+          </button>
         </div>
-        <div className={`flex flex-col-reverse justify-center ${showDriversStandings ? `md:flex-row md:align-center` : `md:flex-row-reverse md:align-center`}`}>
-            {/* show appropriate table based on showDriversStandings */}
-            {showDriversStandings ? <DriverStandingsTable standings={driversStandings} /> : <ConstructorStandingsTable standings={constructorsStandings} />}
+      </div>
+      {/* desktop buttons */}
+      <div className="hidden justify-center md:flex">
+        <div
+          className="basis-1/2"
+          onClick={() => setShowDriversStandings(true)}
+        >
+          <button
+            className={`${
+              showDriversStandings
+                ? `text-2xl border-b-4 w-full border-red-500 rounded`
+                : `text-xl`
+            }`}
+            onClick={() => setShowDriversStandings(true)}
+          >
+            Drivers
+          </button>
+        </div>
+        <div
+          className="basis-1/2"
+          onClick={() => setShowDriversStandings(false)}
+        >
+          <button
+            className={`${
+              !showDriversStandings
+                ? `text-2xl border-b-4 w-full border-red-500 rounded`
+                : `text-xl`
+            }`}
+            onClick={() => setShowDriversStandings(false)}
+          >
+            Constructors
+          </button>
+        </div>
+      </div>
+      <div
+        className={`flex flex-col-reverse justify-center ${
+          showDriversStandings
+            ? `md:flex-row md:align-center`
+            : `md:flex-row-reverse md:align-center`
+        }`}
+      >
+        {/* show appropriate table based on showDriversStandings */}
+        {showDriversStandings ? (
+          <DriverStandingsTable standings={driversStandings} />
+        ) : (
+          <ConstructorStandingsTable standings={constructorsStandings} />
+        )}
 
-            {/* show appropriate podium based on showDriversStandings */}
-            { firstPlaceC && firstPlaceD && showDriversStandings ? <DriverPodium firstPlace={firstPlaceD} secondPlace={secondPlaceD} thirdPlace={thirdPlaceD} />
-            : <ConstructorPodium firstPlace={firstPlaceC} secondPlace={secondPlaceC} thirdPlace={thirdPlaceC} />}
-        </div>
+        {/* show appropriate podium based on showDriversStandings */}
+        {firstPlaceC && firstPlaceD && showDriversStandings ? (
+          <DriverPodium
+            firstPlace={firstPlaceD}
+            secondPlace={secondPlaceD}
+            thirdPlace={thirdPlaceD}
+          />
+        ) : (
+          <ConstructorPodium
+            firstPlace={firstPlaceC}
+            secondPlace={secondPlaceC}
+            thirdPlace={thirdPlaceC}
+          />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StandingsPage
+export default StandingsPage;
